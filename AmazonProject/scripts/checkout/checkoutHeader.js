68001860192e1ebb8dc5f89c529b09de9d367d7c
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-06-03 21:12:22 +0530

    Add checkout header renderer
    Introduce renderCheckoutHeader in checkoutHeader.js to render the checkout page header. It imports calculateCartQuantity to display the current cart item count, injects logo images, a link back to the home page, and a lock icon, and sets the content into the .js-checkout-header element.

---
+++ AmazonProject/scripts/checkout/checkoutHeader.js
@@ -0,0 +1,39 @@
+import { calculateCartQuantity } from "../../data/cart.js";
+
+
+export default function renderCheckoutHeader(){
+    let html = '';
+    const cartQuantity = calculateCartQuantity();
+    
+    html+=
+    `
+        <div class="header-content">
+            
+            <div class="checkout-header-left-section">
+
+                <a href="amazon.html">
+                    <img class="amazon-logo" src="images/amazon-logo.png">
+                    <img class="amazon-mobile-logo" src="images/amazon-mobile-logo.png">
+                </a>
+
+            </div>
+
+            <div class="checkout-header-middle-section">
+
+                Checkout (<a class="return-to-home-link js-return-to-home-link"
+                    href="amazon.html">${cartQuantity}</a>) 
+
+            </div>
+
+            <div class="checkout-header-right-section">
+
+                <img src="images/icons/checkout-lock-icon.png">
+
+            </div>
+
+        </div>
+    `;
+
+    document.querySelector('.js-checkout-header').innerHTML = html;
+                        
+}
