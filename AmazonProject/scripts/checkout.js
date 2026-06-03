0857271131b6c589983e6bd7d22730b372419d9f
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-06-03 21:13:06 +0530

    Update checkout.js
    add call to  renderCheckoutHeader function in checkout.js

--- AmazonProject/scripts/checkout.js
+++ AmazonProject/scripts/checkout.js
@@ -1,5 +1,8 @@
+import renderCheckoutHeader from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
+
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
