cf950a220f87efc6448f793bdf82c60d936f66a4
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-06-04 21:09:44 +0530

    Use shared delivery date helper
    Replace inline dayjs calculations in orderSummary with a shared delivery date helper. orderSummary.js now imports calculateDeliveryDate from deliveryOptions and uses it in place of repeated dayjs logic for rendering delivery dates. checkout.js adds a dayjs import and a calculateDelivery function (exported and briefly logged) to provide/validate delivery date computation.

--- AmazonProject/scripts/checkout.js
+++ AmazonProject/scripts/checkout.js
@@ -1,8 +1,21 @@
 import renderCheckoutHeader from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
+
+export function calculateDelivery(d){
+
+        const today = dayjs();
+        
+        const deliveryDate = today.add(d,'day');
+        const day = deliveryDate.format('dddd');
+        return deliveryDate.format('dddd, MMMM D');
+
+}
+
+const ans = calculateDelivery(2);
+console.log(ans);
--- AmazonProject/scripts/checkout/orderSummary.js
+++ AmazonProject/scripts/checkout/orderSummary.js
@@ -2,8 +2,7 @@ import { cart, updateDeliveryOption, updateQuantity, calculateCartQuantity } fro
 import { getProduct, products } from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js'
 import { removeFromCart } from '../../data/cart.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { calculateDeliveryDate, deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import renderCheckoutHeader from './checkoutHeader.js';
 
@@ -23,9 +22,8 @@ export function renderOrderSummary(){
         const  deliveryOption =  getDeliveryOption(deliveryOptionId);
 
         console.log("The delivery option is: " , deliveryOption);
-        const today = dayjs();
-        const deliveryDate = today.add(deliveryOption.deliveryDays,'day');
-        const dateString = deliveryDate.format('dddd, MMMM D');
+        
+        const dateString = calculateDeliveryDate(deliveryOption);
 
 
         cartSummaryHTML+=`
@@ -99,9 +97,7 @@ export function renderOrderSummary(){
 
         deliveryOptions.forEach((deliveryOption)=>{
 
-            const today = dayjs();
-            const deliveryDate = today.add(deliveryOption.deliveryDays,'day');
-            const dateString = deliveryDate.format('dddd, MMMM D');
+            const dateString = calculateDeliveryDate(deliveryOption);
             const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`
             const isChecked = cartItem.deliveryOptionId === deliveryOption.id;
 
