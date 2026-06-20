370d5e0400f53fc6ff7ccbd050008fc23c37e3e0
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-06-20 21:08:02 +0530

    Add Clothing subclass and size chart link
    Introduce a Clothing class that extends Product, adds a sizeChartLink property, and overrides extraInfoHTML to render a Size Chart link. Add a default extraInfoHTML method to Product. Update the products factory to instantiate Clothing when productDetails.type === 'clothing'. Render the extraInfoHTML in the product template so clothing items display the size chart link.

--- AmazonProject/data/products.js
+++ AmazonProject/data/products.js
@@ -37,6 +37,26 @@ class Product{
   getPrice(){
     return `$${formatCurrency(this.priceCents)}`
   }
+
+  extraInfoHTML(){
+    return '';
+  }
+}
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+      super(productDetails);
+      this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+      <a href="${this.sizeChartLink}" target="_blank"> 
+      Size Chart
+      </a>
+    `
+  }
 }
 
 
@@ -700,5 +720,11 @@ export const products = [
     ]
   }
 ].map((productDetails) =>{
-  return new Product(productDetails);
+
+    if(productDetails.type === 'clothing'){
+
+      return new Clothing(productDetails);
+    }
+
+    return new Product(productDetails);
 })
