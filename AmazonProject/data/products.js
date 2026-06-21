1c3ba008ec14ed39722090962a3642c8692dd5ca
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-06-21 14:44:08 +0530

    Add appliance product class and data
    Introduce an Applicance class extending Product that adds instruction and warranty links and renders them via extraInfoHTML. Update several product entries to set type: "appliance" and include instructionLink and warrantyLink assets. Update the product factory to instantiate Applicance for products with type "appliance". Note: the class name is spelled "Applicance" in this change (possible typo).

--- AmazonProject/data/products.js
+++ AmazonProject/data/products.js
@@ -60,6 +60,28 @@ class Clothing extends Product{
 }
 
 
+class Applicance extends Product{
+
+    instructionLink;
+    warrantyLink;
+
+    constructor(productDetails){
+
+      super(productDetails);
+      this.instructionLink = productDetails.instructionLink;
+      this.warrantyLink = productDetails.warrantyLink;
+    }
+
+
+    extraInfoHTML(){
+      return `
+      <a href="${this.instructionLink}" target="_blank" >Instructions</a>
+      <a href="${this.warrantyLink}" target="_blank" >Warranty</a>
+      `;
+    }
+}
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -120,7 +142,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -305,7 +330,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "appliance",
+    instructionLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -610,7 +638,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -670,7 +701,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "appliance",
+    instructionLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -724,7 +758,9 @@ export const products = [
     if(productDetails.type === 'clothing'){
 
       return new Clothing(productDetails);
+    }else if(productDetails.type === 'appliance'){
+      return new Applicance(productDetails);
     }
 
-    return new Product(productDetails);
+      return new Product(productDetails);
 })
